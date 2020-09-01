abf08aa3d4744552c0bf7eb272f191866f9d0d0c
Akihiro Takemura <5753479+atakemura@users.noreply.github.com>
2020-09-02 00:07:37 +0900

    add data loading to util

--- tree_asp/utils.py
+++ tree_asp/utils.py
@@ -1,8 +1,53 @@
+import json
+import pandas as pd
+
 from contextlib import contextmanager
 from timeit import default_timer as timer
+from sklearn.datasets import load_iris, load_breast_cancer, load_wine
+from pathlib import Path
+
 
 @contextmanager
 def timer_exec(name):
     start = timer()
     yield
     print('{} time elapsed {} seconds'.format(name, round(timer() - start)))
+
+
+def load_data(dataset_name):
+    # there is no categorical feature in these sklearn datasets.
+    sklearn_data = {'iris': load_iris,
+                    'breast_sk': load_breast_cancer,
+                    'wine': load_wine}
+    # the following contains a mix of categorical and numerical features.
+    datasets = ['autism', 'breast', 'cars',
+                'credit_australia', 'credit_taiwan', 'heart', 'ionosphere',
+                'kidney', 'krvskp', 'voting',
+                # these datasets are large (>200k), use with caution
+                'census', 'airline', 'kdd99']
+    if dataset_name in sklearn_data.keys():
+        load_data_method = sklearn_data[dataset_name]
+        data_obj = load_data_method()
+        feat = data_obj['feature_names']
+        data = data_obj['data']
+        target = data_obj['target']
+
+        df = pd.DataFrame(data, columns=feat).assign(target=target)
+        X, y = df[feat], df['target']
+        dataset = (X, y)
+    elif dataset_name in datasets:
+        dataset_dir = Path('../datasets/datasets/') / dataset_name
+
+        raw = pd.read_csv(Path(dataset_dir / dataset_name).with_suffix('.csv'))
+        with open(dataset_dir / 'schema.json', 'r') as infile:
+            schema = json.load(infile)
+        for c in schema['categorical_columns']:
+            raw.loc[:, c] = raw.loc[:, c].astype('category')
+        raw_x = raw[[c for c in raw.columns if c != schema['label_column']]].copy()
+        if schema['id_col'] != "":
+            raw_x.drop(schema['id_col'], axis=1, inplace=True)
+        raw_y = raw[schema['label_column']]
+        dataset = (raw_x, raw_y)
+    else:
+        raise ValueError('unrecognized dataset name: {}'.format(dataset_name))
+    return dataset
